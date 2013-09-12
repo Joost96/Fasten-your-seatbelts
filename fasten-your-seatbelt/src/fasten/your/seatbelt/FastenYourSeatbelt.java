555d7d6ad4b48cebb88dd7c8c82731d4a57f148b
Joost96 <joost-k@live.nl>
2013-09-12 12:56:14 +0200

    author aangepast
    author joost aangepast naar author team V

--- fasten-your-seatbelt/src/fasten/your/seatbelt/FastenYourSeatbelt.java
+++ fasten-your-seatbelt/src/fasten/your/seatbelt/FastenYourSeatbelt.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package fasten.your.seatbelt;
 
 /**
  *
- * @author Joost
+ * @author Team V
  */
 public class FastenYourSeatbelt
 {
